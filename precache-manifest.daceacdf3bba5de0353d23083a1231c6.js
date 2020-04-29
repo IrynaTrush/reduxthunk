86f4d4f3d1fc5885f5bdce2ae980c97a4141a5ee
Iryna <irynka1071994@gmail.com>
2020-04-29 22:57:45 +0300

    Updates

---
+++ precache-manifest.daceacdf3bba5de0353d23083a1231c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "256d4417561c3f9c1af47e4ca989c04b",
+    "url": "/reduxthunk/index.html"
+  },
+  {
+    "revision": "32e7df60a86297bdc6e1",
+    "url": "/reduxthunk/static/css/main.6632370e.chunk.css"
+  },
+  {
+    "revision": "1cde60426d61b541b8b3",
+    "url": "/reduxthunk/static/js/2.22bfe4b8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reduxthunk/static/js/2.22bfe4b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32e7df60a86297bdc6e1",
+    "url": "/reduxthunk/static/js/main.f94bf9aa.chunk.js"
+  },
+  {
+    "revision": "2109b618d8764a3ce1b2",
+    "url": "/reduxthunk/static/js/runtime-main.4766916e.js"
+  }
+]);
